197945dc9b1f81639a006fafc59d9384d35e5510
Flowers for Tuesday <xuuumz@gmail.com>
2025-08-08 16:08:32 +0800

    2D Collision Detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     private float xinput;
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpforce = 5f;
+    [SerializeField] private float groundCheckDistance = 0.1f;
+    [SerializeField] private LayerMask groundLayer;
     private Rigidbody2D rb;
     private Animator anim;
     // Start is called before the first frame update
@@ -28,10 +30,7 @@ public class Player : MonoBehaviour
 
     private void CheckInput()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.K))&&IsGrounded() )   Jump();
     }
 
     private void AnimatorContollers()
@@ -45,10 +44,18 @@ public class Player : MonoBehaviour
         xinput = Input.GetAxisRaw("Horizontal");
         // Debug.Log("X Input: " + xinput);
         rb.velocity = new Vector2(xinput * speed, rb.velocity.y);
+        if (xinput > 0) transform.localScale = new Vector3(1, 1, 1);
+        else if (xinput < 0) transform.localScale = new Vector3(-1, 1, 1);
     }
 
     private void Jump()
     {
         rb.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        return hit.collider != null;
     }
 }
